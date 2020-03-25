6e134b78a6806d2f02b158ce32706567ba26c0e3
Jon davi <stg77713@gmail.com>
2020-03-25 01:38:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,7 +70,7 @@ class TicTacToe
   
   def won?
     WIN_COMBINATIONS.detect do |win_combination|
-      @board[win_combination[0]] == @board[win_combination[1]] && @board[win_combination[1]] == @board[win_combination[2]] && position_taken?(win_combination[0])]
+      @board[win_combination[0]] == @board[win_combination[1]] && @board[win_combination[1]] == @board[win_combination[2]] && position_taken?(win_combination[0])
     end 
   end 
   
